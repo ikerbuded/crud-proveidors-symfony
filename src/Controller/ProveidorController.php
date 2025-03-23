017a586fab65e13253a52eb5ef5f4eacfbfdf94c
Iker Buded <iker.buded6@gmail.com>
2025-03-23 15:10:44 +0100

    Actualització de format en vistes i creació de les funcions index, create i store del al Contolador

--- src/Controller/ProveidorController.php
+++ src/Controller/ProveidorController.php
@@ -2,19 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Proveidor;
+use App\Repository\ProveidorRepository;
+use DateTime;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProveidorController extends AbstractController
 {
     /**
-     * @Route("/proveidor", name="app_proveidor")
+     * @Route("/", name="app_index_proveidor")
      */
-    public function index(): Response
+
+    public function index(ProveidorRepository $proveidorRepository): Response
     {
+        $proveidors = $proveidorRepository->findAll();
         return $this->render('proveidor/index.html.twig', [
-            'controller_name' => 'ProveidorController',
+            'proveidors' => $proveidors,
         ]);
+    }
+
+    /**
+     * @Route("/afegir", name="app_afegir_proveidor")
+     */
+    public function create(): Response
+    {
+        return $this->render('proveidor/create.html.twig');
+    }
+
+    /**
+     * @Route("/store", name="app_guardar_proveidor")
+     */
+    public function store(Request $request): Response
+    {
+        $proveidor = new Proveidor();
+        $proveidor->setNom($request->request->get('nom'));
+        $proveidor->setEmail($request->request->get('email'));
+        $proveidor->setTelefon($request->request->get('telefon'));
+        $proveidor->setTipus($request->request->get('tipus'));
+        $proveidor->setActiu($request->request->get('actiu') === 'on');
+        $proveidor->setDataCreacio(new DateTimeImmutable());
+        $proveidor->setDataActualitzacio(new DateTime());
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($proveidor);
+        $em->flush();
+        $this->addFlash('sucess', 'Proveidor afegit correctament!');
+        return $this->redirectToRoute('app_index_proveidor');
     }
 }
